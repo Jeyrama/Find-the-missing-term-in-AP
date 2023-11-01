6654209c5b4653fbdd1e0ffe4fb78225b32fc4f1
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-31 21:17:15 -0700

    Update Find-the-missing-term-in-AP.js

--- Find-the-missing-term-in-AP.js
+++ Find-the-missing-term-in-AP.js
@@ -24,3 +24,18 @@ let findMissing = function (list) {
   let sum = list.reduce(function(acc, x) { return acc + x; });
   return expected_sum - sum;
 }
+
+// or
+
+let findMissingTerm = function (list) { 
+  let delta1 = list[0] - list[1];
+  let delta2 = list[1] - list[2];
+  let delta  = Math.min(Math.abs(delta1), Math.abs(delta2));
+  let direction = list[0] < list[1] ? 1 : -1;
+  
+  let start = list[0];
+  
+  for (let i = 0; i < list.length; i++, start += delta * direction) {
+    if (list[i] !== start) return start;  
+  }
+}
